442d7f20252e0ea6dff3272512b52734bd0e4866
Dima Bognen <34019262+bognen@users.noreply.github.com>
2020-07-26 17:21:20 -0600

    Removed sensitive data

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,8 +3,6 @@ import App from './App.vue'
 import router from './router'
 
 Vue.config.productionTip = false
-Vue.prototype.$apiURI = "http://18.191.43.124:8000"
-Vue.prototype.$rapidKey = "57c7a0bfecmsh6bd72873440fb7ep1be0bfjsned54b9a32281"
 
 new Vue({
   router,
